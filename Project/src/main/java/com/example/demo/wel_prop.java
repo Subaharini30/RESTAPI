6e3dbdd96f17f2b44ed6906ee68e0fa4b635c24f
Subaharini-3008 <727721euit161@skcet.ac.in>
2023-02-21 20:22:37 +0530

    RESTAPI

---
+++ Project/src/main/java/com/example/demo/wel_prop.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class wel_prop 
+{
+	@Value("${greetings.message}")
+	private String welcome;
+	
+	@GetMapping("/prop")
+	public String welcome()
+	{
+		return welcome;
+	}
+}
