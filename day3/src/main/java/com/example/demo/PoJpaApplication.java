a4fb2887e04b59dd8898ac0f0d965cb1540e45ba
Subaharini-3008 <727721euit161@skcet.ac.in>
2023-02-23 15:32:10 +0530

    RestApi_day3

---
+++ day3/src/main/java/com/example/demo/PoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoJpaApplication.class, args);
+	}
+
+}
